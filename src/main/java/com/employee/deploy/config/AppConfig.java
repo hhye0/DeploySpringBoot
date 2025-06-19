f8cee8159a7a9681140b83742b9be0643c62a585
hhye0 <jhy030123@naver.com>
2025-06-20 06:12:56 +0900

    50일차

---
+++ src/main/java/com/employee/deploy/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.employee.deploy.config;
+
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    Hibernate6Module hibernate6Module() {
+        Hibernate6Module hibernate6Module = new Hibernate6Module();
+        hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING,true);
+        return hibernate6Module;
+    }
+}
